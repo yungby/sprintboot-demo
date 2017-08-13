253faff1a5d1b6736d5ddae9d9af9f1ba443dd0d
yungby <yungby@DESKTOP-QFM0RTG>
2017-08-13 23:30:31 +0800

    remove no used package

--- src/main/java/com/example/demo/timer/service/TimerTask.java
+++ src/main/java/com/example/demo/timer/service/TimerTask.java
@@ -6,19 +6,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TimerTask {
-
-//	@Scheduled(cron="*/6 * * * * ?")
-	public void timeTask1(){
+	// @Scheduled(cron="*/6 * * * * ?")
+	public void timeTask1() {
 		System.out.println("timeTask1 " + LocalTime.now());
 	}
-	
-//	@Scheduled(fixedRate = 5000)
-	public void timeTask2(){
+
+	// @Scheduled(fixedRate = 5000)
+	public void timeTask2() {
 		System.out.println("timeTask2 " + LocalTime.now());
 	}
-	
-//	@Scheduled(initialDelay=2000, fixedRate=8000)    
-	public void timeTask3(){
+
+	// @Scheduled(initialDelay=2000, fixedRate=8000)
+	public void timeTask3() {
 		System.out.println("timeTask3 " + LocalTime.now());
 	}
 }
